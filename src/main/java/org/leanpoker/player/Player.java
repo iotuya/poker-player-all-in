c6eb60ee66768e9dd56b83035af62d60c3247dea
tmikulcek <tmikulcek@gmail.com>
2015-04-25 10:25:40 +0200

    fix buyIn

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,8 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.Map;
 
@@ -10,7 +12,17 @@ public class Player {
 
     public static int betRequest(JsonElement request) {
         int buyIn = request.getAsJsonObject().get("current_buy_in").getAsInt();
+        JsonArray players = request.getAsJsonObject().get("playeres").getAsJsonArray();
         int round = request.getAsJsonObject().get("round").getAsInt();
+        int playerBet = 0;
+        for (JsonElement player : players) {
+            JsonObject playerObject = player.getAsJsonObject();
+            if ("active".equals(playerObject.get("status"))) {
+                playerBet = playerObject.get("bet").getAsInt();
+                break;
+            }
+        }
+        buyIn = buyIn - playerBet;
         return round == 0 ? buyIn : buyIn * 2;
     }
 
